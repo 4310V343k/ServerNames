3362c1dcfaad934ab9590ea609357ea2f095bd15
y7o4ka <56449763+4310V343k@users.noreply.github.com>
2022-12-24 22:31:51 +0300

    EXILED 6 (#1)

--- Config.cs
+++ Config.cs
@@ -1,13 +1,17 @@
-﻿using Exiled.API.Interfaces;
-using System.ComponentModel;
-
-namespace ServerNames
+﻿namespace ServerNames
 {
+    using System.ComponentModel;
+
+    using Exiled.API.Interfaces;
+
     public class Config : IConfig
     {
-        [Description("Should the override be enabled?")]
-        public bool IsEnabled { get; set; } = false;
         [Description("If yes, what would the new server name be?")]
         public string Name { get; set; } = "Example";
+
+        [Description("Should the override be enabled?")]
+        public bool IsEnabled { get; set; } = false;
+
+        public bool Debug { get; set; } = false;
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,11 +1,14 @@
-﻿using Exiled.API.Features;
-using System;
-using Server = Exiled.Events.Handlers.Server;
-
-namespace ServerNames
+﻿namespace ServerNames
 {
+    using System;
+
+    using Exiled.API.Features;
+
+    using Server = Exiled.Events.Handlers.Server;
+
     public class ServerNames : Plugin<Config>
     {
+        private EventHandlers eventHandlers;
         public override string Author => "Remind me";
         public override string Name => "ServerNames";
         public override string Prefix => "Server Names";
@@ -13,14 +16,13 @@ namespace ServerNames
         public override Version RequiredExiledVersion => new Version(2, 3, 3);
         public static ServerNames Instance { get; } = new ServerNames();
 
-        private EventHandlers eventHandlers;
-
         public override void OnEnabled()
         {
             eventHandlers = new EventHandlers();
             Server.WaitingForPlayers += eventHandlers.OnWaitingForPlayers;
             base.OnEnabled();
         }
+
         public override void OnDisabled()
         {
             Server.WaitingForPlayers -= eventHandlers.OnWaitingForPlayers;
